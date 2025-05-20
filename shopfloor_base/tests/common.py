camptocamp/wms

shopfloor_base: fix common setup

shopfloor_base/tests/common.py
@@ -56,10 +56,13 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
 
     @contextmanager
     def work_on_services(self, collection=None, env=None, **params):
+        collection = collection or self.shopfloor_app
+        if env:
+            collection = collection.with_env(env)
         params = params or {}
         yield WorkContext(
             model_name="rest.service.registration",
-            collection=collection or self.shopfloor_app,
+            collection=collection,
             # No need for a real request mock
             # as we don't deal w/ real request for testing
             # but base_rest context provider needs it.
@@ -67,8 +70,8 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
             **params
         )
 
-    def get_service(self, usage, env=None, **kw):
-        with self.work_on_services(env=env, **kw) as work:
+    def get_service(self, usage, collection=None, env=None, **kw):
+        with self.work_on_services(collection=collection, env=env, **kw) as work:
             service = work.component(usage=usage)
             # Thanks to shopfloor.app we don't need controllers
             # but not having a controller means that non decorated methods
@@ -103,15 +106,14 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
                 _service_skip_request_validation=True,
             )
         )
-
-        cls.shopfloor_app = cls.env["shopfloor.app"].search(
-            [("tech_name", "=", "default")], limit=1
-        )
         cls.setUpComponent()
         cls.setUpRegistry()
         cls.setUpClassUsers()
         cls.setUpClassVars()
         cls.setUpClassBaseData()
+
+        # Keep this here to have the whole env set up already
+        cls.setUpShopfloorApp()
 
         with cls.work_on_actions(cls) as work:
             cls.data = work.component(usage="data")
@@ -138,6 +140,7 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
             "groups_id": [
                 (6, 0, [cls.env.ref("shopfloor_base.group_shopfloor_user").id])
             ],
+            "tz": cls.env.user.tz,
         }
 
     @classmethod
@@ -149,6 +152,7 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
             "groups_id": [
                 (6, 0, [cls.env.ref("shopfloor_base.group_shopfloor_manager").id])
             ],
+            "tz": cls.env.user.tz,
         }
 
     @classmethod
@@ -158,6 +162,21 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
     @classmethod
     def setUpClassBaseData(cls):
         pass
+
+    @classmethod
+    def setUpShopfloorApp(cls):
+        cls.shopfloor_app = (
+            cls.env["shopfloor.app"]
+            .sudo(cls.shopfloor_manager)
+            .create(
+                {
+                    "tech_name": "test",
+                    "name": "Test",
+                    "short_name": "test",
+                }
+            )
+            .with_user(cls.shopfloor_user)
+        )
 
     def assert_response(
         self, response, next_state=None, message=None, data=None, popup=None

OCA/shopfloor-app

shopfloor_base/tests/common.py
@@ -56,10 +56,13 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
 
     @contextmanager
     def work_on_services(self, collection=None, env=None, **params):
+        collection = collection or self.shopfloor_app
+        if env:
+            collection = collection.with_env(env)
         params = params or {}
         yield WorkContext(
             model_name="rest.service.registration",
-            collection=collection or self.shopfloor_app,
+            collection=collection,
             # No need for a real request mock
             # as we don't deal w/ real request for testing
             # but base_rest context provider needs it.
@@ -67,8 +70,8 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
             **params
         )
 
-    def get_service(self, usage, env=None, **kw):
-        with self.work_on_services(env=env, **kw) as work:
+    def get_service(self, usage, collection=None, env=None, **kw):
+        with self.work_on_services(collection=collection, env=env, **kw) as work:
             service = work.component(usage=usage)
             # Thanks to shopfloor.app we don't need controllers
             # but not having a controller means that non decorated methods
@@ -103,15 +106,14 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
                 _service_skip_request_validation=True,
             )
         )
-
-        cls.shopfloor_app = cls.env["shopfloor.app"].search(
-            [("tech_name", "=", "default")], limit=1
-        )
         cls.setUpComponent()
         cls.setUpRegistry()
         cls.setUpClassUsers()
         cls.setUpClassVars()
         cls.setUpClassBaseData()
+
+        # Keep this here to have the whole env set up already
+        cls.setUpShopfloorApp()
 
         with cls.work_on_actions(cls) as work:
             cls.data = work.component(usage="data")
@@ -138,6 +140,7 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
             "groups_id": [
                 (6, 0, [cls.env.ref("shopfloor_base.group_shopfloor_user").id])
             ],
+            "tz": cls.env.user.tz,
         }
 
     @classmethod
@@ -149,6 +152,7 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
             "groups_id": [
                 (6, 0, [cls.env.ref("shopfloor_base.group_shopfloor_manager").id])
             ],
+            "tz": cls.env.user.tz,
         }
 
     @classmethod
@@ -158,6 +162,21 @@ class CommonCase(SavepointCase, RegistryMixin, ComponentMixin):
     @classmethod
     def setUpClassBaseData(cls):
         pass
+
+    @classmethod
+    def setUpShopfloorApp(cls):
+        cls.shopfloor_app = (
+            cls.env["shopfloor.app"]
+            .with_user(cls.shopfloor_manager)
+            .create(
+                {
+                    "tech_name": "test",
+                    "name": "Test",
+                    "short_name": "test",
+                }
+            )
+            .with_user(cls.shopfloor_user)
+        )
 
     def assert_response(
         self, response, next_state=None, message=None, data=None, popup=None