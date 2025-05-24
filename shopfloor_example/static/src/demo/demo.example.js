a520f05ab301b35ed74ff63cba72b1838e754d44
Simone Orsi <simahawk@gmail.com>
2025-05-24 11:56:14 +0200

    shopfloor_example: drop useless wms static dir

--- shopfloor_example/static/src/demo/demo.example.js
+++ shopfloor_example/static/src/demo/demo.example.js
@@ -5,7 +5,7 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
-import {demotools} from "/shopfloor_mobile_base/static/wms/src/demo/demo.core.js";
+import {demotools} from "/shopfloor_mobile_base/static/src/demo/demo.core.js";
 
 const DEMO_CASE = {
     by_menu_id: {},
