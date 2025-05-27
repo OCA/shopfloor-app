camptocamp/wms

sf_mobile_base: fix home translations

shopfloor_mobile_base/static/wms/src/i18n/i18n.fr.js
@@ -9,11 +9,12 @@ import {translation_registry} from "../services/translation_registry.js";
 const messages_fr = {
     screen: {
         home: {
-            title: "Barcode scanner",
+            title: "Home",
+            main_title: "Home",
         },
         scan_anything: {
             name: "Scanner",
-            title: "Scanner {what}",
+            title: "Scan {what}",
         },
         settings: {
             home: {

OCA/shopfloor-app

shopfloor_mobile_base/static/wms/src/i18n/i18n.fr.js
@@ -9,11 +9,12 @@ import {translation_registry} from "../services/translation_registry.js";
 const messages_fr = {
     screen: {
         home: {
-            title: "Barcode scanner",
+            title: "Accueil",
+            main_title: "Accueil",
         },
         scan_anything: {
             name: "Scanner",
-            title: "Scanner {what}",
+            title: "Scannez {what}",
         },
         settings: {
             home: {